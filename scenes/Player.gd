216accd751f3099cdc8564062bd5200d55ac36d7
Pudim <aalistalker@gmail.com>
2022-03-10 20:23:17 -0300

    Changes to the player controller and Gun

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,65 +1,53 @@
 extends KinematicBody
 
-#signals
-signal changed_ammo(magammo)
-
-# stats
-var curHP : int = 2
-var maxHP : int = 2
-var ammo : int = 0
-var magammo : int = 15
-const SWAY = 30
-const damage = 10
-var holstered = false
-
- 
-# physics
-var speed = 10
+var speed = 7
 const ACCEL_DEFAULT = 7
 const ACCEL_AIR = 1
 onready var accel = ACCEL_DEFAULT
-var jump = 10
-var gravity : float = 20.0
-const MAX_CAM_SHAKE = 0.6
+var gravity = 9.8
+var jump = 5
 
- 
-# cam look
 var cam_accel = 40
-var mouse_sense = 1
+var mouse_sense = 0.3
 var snap
 
-# vectors
 var direction = Vector3()
-var h_velocity = Vector3()
-var h_acceleration = 6
 var velocity = Vector3()
 var gravity_vec = Vector3()
 var movement = Vector3()
 
-# components
-onready var raycast = $head/Cam/GunCast
-onready var anim_player = $AnimationPlayer
-onready var hand = $head/Cam/hand
 onready var head = $head
-onready var handloc = $head/handloc
 onready var camera = $head/Cam
-onready var guncam = $head/Cam/ViewportContainer/Viewport/Guncam
-onready var b_decal = preload("res://scenes/BulletDecal.tscn")
-onready var sfxgun = $SFXGun
-onready var sfxemptygun = $SFXEmptyGun
-onready var anim_tree = $AnimationTree
-onready var sfxwalkingsnow = $WalkingSnow
- 
-func _ready():
-	hand.set_as_toplevel(true)
-	# hide and lock the mouse cursor
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	
 
+func _ready():
+	#hides the cursor
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	#get mouse input for camera rotation
+	if event is InputEventMouseMotion:
+		rotate_y(deg2rad(-event.relative.x * mouse_sense))
+		head.rotate_x(deg2rad(-event.relative.y * mouse_sense))
+		head.rotation.x = clamp(head.rotation.x, deg2rad(-89), deg2rad(89))
+
+func _process(delta):
+	#camera physics interpolation to reduce physics jitter on high refresh-rate monitors
+	if Engine.get_frames_per_second() > Engine.iterations_per_second:
+		camera.set_as_toplevel(true)
+		camera.global_transform.origin = camera.global_transform.origin.linear_interpolate(head.global_transform.origin, cam_accel * delta)
+		camera.rotation.y = rotation.y
+		camera.rotation.x = head.rotation.x
+	else:
+		camera.set_as_toplevel(false)
+		camera.global_transform = head.global_transform
+		
 func _physics_process(delta):
 	#get keyboard input
-	direction = Vector3()
-	
+	direction = Vector3.ZERO
+	var h_rot = global_transform.basis.get_euler().y
+	var f_input = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
+	var h_input = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	direction = Vector3(h_input, 0, f_input).rotated(Vector3.UP, h_rot).normalized()
 	
 	#jumping and gravity
 	if is_on_floor():
@@ -75,52 +63,14 @@ func _physics_process(delta):
 		snap = Vector3.ZERO
 		gravity_vec = Vector3.UP * jump
 	
-	#Moving and direction
-	if Input.is_action_pressed("move_forward"):
-		direction -= transform.basis.z
-	elif Input.is_action_pressed("move_backward"):
-		direction += transform.basis.z
-	if Input.is_action_pressed("move_left"):
-		direction -= transform.basis.x
-	elif Input.is_action_pressed("move_right"):
-		direction += transform.basis.x
+	#make it move
+	velocity = velocity.linear_interpolate(direction * speed, accel * delta)
+	movement = velocity + gravity_vec
 	
-	direction = direction.normalized()
-	h_velocity = h_velocity.linear_interpolate(direction * speed, h_acceleration * delta)
-	movement.z = h_velocity.z + gravity_vec.z
-	movement.x = h_velocity.x + gravity_vec.x
-	movement.y = gravity_vec.y
-	
-	move_and_slide(movement, Vector3.UP)
-
-
-func _process(delta):
-	
-#	fire()
-	
-	guncam.global_transform = camera.global_transform
-	
-	hand.global_transform.origin = handloc.global_transform.origin
-	hand.rotation.y = lerp_angle(hand.rotation.y, rotation.y, SWAY * delta)
-	hand.rotation.x = lerp_angle(hand.rotation.x, head.rotation.x, SWAY * delta)
+	move_and_slide_with_snap(movement, snap, Vector3.UP)
 	
 	
 	
-	if Engine.get_frames_per_second() > Engine.iterations_per_second:
-		camera.set_as_toplevel(true)
-		camera.global_transform.origin = camera.global_transform.origin.linear_interpolate(head.global_transform.origin, cam_accel * delta)
-		camera.rotation.y = rotation.y
-		camera.rotation.x = head.rotation.x
-	else:
-		camera.set_as_toplevel(false)
-		camera.global_transform = head.global_transform
- 
-func _input(event):
-	
-	if event is InputEventMouseMotion:
-		rotate_y(deg2rad(-event.relative.x * mouse_sense))
-		head.rotate_x(deg2rad(-event.relative.y * mouse_sense))
-		head.rotation.x = clamp(head.rotation.y, deg2rad(-89), deg2rad(89))
 
 
 
@@ -135,3 +85,145 @@ func _input(event):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+#extends KinematicBody
+#
+##signals
+#signal changed_ammo(magammo)
+#
+## stats
+#var curHP : int = 2
+#var maxHP : int = 2
+#var ammo : int = 0
+#var magammo : int = 15
+#const SWAY = 30
+#const damage = 10
+#var holstered = false
+#
+#
+## physics
+#var speed = 10
+#const ACCEL_DEFAULT = 7
+#const ACCEL_AIR = 1
+#onready var accel = ACCEL_DEFAULT
+#var jump = 10
+#var gravity : float = 20.0
+#const MAX_CAM_SHAKE = 0.6
+#
+#
+## cam look
+#var cam_accel = 40
+#var mouse_sense = 1
+#var snap
+#
+## vectors
+#var direction = Vector3()
+#var h_velocity = Vector3()
+#var h_acceleration = 6
+#var velocity = Vector3()
+#var gravity_vec = Vector3()
+#var movement = Vector3()
+#
+## components
+#onready var anim_player = $AnimationPlayer
+#onready var hand = $head/Cam/hand
+#onready var head = $head
+#onready var camera = $head/Cam
+#onready var b_decal = preload("res://scenes/BulletDecal.tscn")
+#
+#
+#func _ready():
+#	hand.set_as_toplevel(true)
+#	# hide and lock the mouse cursor
+#	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+#
+#
+#func _physics_process(delta):
+#	#get keyboard input
+#	direction = Vector3()
+#
+#
+#	#jumping and gravity
+#	if is_on_floor():
+#		snap = -get_floor_normal()
+#		accel = ACCEL_DEFAULT
+#		gravity_vec = Vector3.ZERO
+#	else:
+#		snap = Vector3.DOWN
+#		accel = ACCEL_AIR
+#		gravity_vec += Vector3.DOWN * gravity * delta
+#
+#	if Input.is_action_just_pressed("jump") and is_on_floor():
+#		snap = Vector3.ZERO
+#		gravity_vec = Vector3.UP * jump
+#
+#	#Moving and direction
+#	if Input.is_action_pressed("move_forward"):
+#		direction -= transform.basis.z
+#	elif Input.is_action_pressed("move_backward"):
+#		direction += transform.basis.z
+#	if Input.is_action_pressed("move_left"):
+#		direction -= transform.basis.x
+#	elif Input.is_action_pressed("move_right"):
+#		direction += transform.basis.x
+#
+#	direction = direction.normalized()
+#	h_velocity = h_velocity.linear_interpolate(direction * speed, h_acceleration * delta)
+#	movement.z = h_velocity.z + gravity_vec.z
+#	movement.x = h_velocity.x + gravity_vec.x
+#	movement.y = gravity_vec.y
+#
+#	move_and_slide(movement, Vector3.UP)
+#
+#
+#func _process(delta):
+#
+##	fire()
+#
+#	#guncam.global_transform = camera.global_transform
+#
+#	#hand.global_transform.origin = handloc.global_transform.origin
+#	#hand.rotation.y = lerp_angle(hand.rotation.y, rotation.y, SWAY * delta)
+#	#hand.rotation.x = lerp_angle(hand.rotation.x, head.rotation.x, SWAY * delta)
+#
+#
+#
+#	if Engine.get_frames_per_second() > Engine.iterations_per_second:
+#		camera.set_as_toplevel(true)
+#		camera.global_transform.origin = camera.global_transform.origin.linear_interpolate(head.global_transform.origin, cam_accel * delta)
+#		camera.rotation.y = rotation.y
+#		camera.rotation.x = head.rotation.x
+#	else:
+#		camera.set_as_toplevel(false)
+#		camera.global_transform = head.global_transform
+#
+#func _input(event):
+#
+#	if event is InputEventMouseMotion:
+#		rotate_y(deg2rad(-event.relative.x * mouse_sense))
+#		head.rotate_x(deg2rad(-event.relative.y * mouse_sense))
+#		head.rotation.x = clamp(head.rotation.y, deg2rad(-89), deg2rad(89))
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
--- scenes/hand.gd
+++ scenes/hand.gd
@@ -1,27 +1,28 @@
 extends Spatial
 
-const ADS_LERP = 20
+var mouse_mov
+var sway_threshold = 5
+var sway_lerp = 5
 
-export var camera_path : NodePath
-var camera : Camera
-
-export var default_position : Vector3
-export var ads_position : Vector3
-var fview = {"Default":65, "ADS":50}
+export var sway_left : Vector3
+export var sway_right : Vector3
+export var sway_normal : Vector3
 
 func _ready():
-	camera = get_node(camera_path)
+	pass
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		mouse_mov = -event.relative.x
 
 func _process(delta):
-	if Input.is_action_pressed("fire2"):
-		transform.origin = transform.origin.linear_interpolate(ads_position, ADS_LERP * delta)
-		camera.fov = lerp(camera.fov, fview["ADS"], ADS_LERP * delta)
-	else:
-		transform.origin = transform.origin.linear_interpolate(default_position, ADS_LERP * delta)
-		camera.fov = lerp(camera.fov, fview["Default"], ADS_LERP * delta)
-	
-	
-	
+	if mouse_mov != null:
+		if mouse_mov > sway_threshold:
+			rotation = rotation.linear_interpolate(sway_left, sway_lerp * delta)
+		elif mouse_mov < -sway_threshold:
+			rotation = rotation.linear_interpolate(sway_right, sway_lerp * delta)
+		else:
+			rotation = rotation.linear_interpolate(sway_normal, sway_lerp * delta)
 	
 	
 	
